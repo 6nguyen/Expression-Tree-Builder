a301b1ca5a2b87c43132dee26215c37e3e3911e5
George Nguyen <g.nguyen92@yahoo.com>
2017-04-24 14:10:02 -0700

    Upload Assignment3.java, the Main activity for the program.

---
+++ src/Assignment3.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment3;
+
+/**
+ *
+ * @author George
+ */
+public class Assignment3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // read postfix expression into tree1.expression
+       ExprTree tree1 = new ExprTree();
+       
+       // create an expression tree from tree1, stored in tree1.root
+       tree1.createExprTree(tree1.expression);
+       
+       // Evaluate expression tree held in root
+       System.out.println(tree1.expression + " = " 
+               + tree1.evaluateExpression(tree1.root) + "\n");
+       
+       // Create second expression tree
+       ExprTree tree2 = new ExprTree();
+       tree2.createExprTree(tree2.expression);
+       System.out.println(tree2.expression + " = " 
+               + tree2.evaluateExpression(tree2.root) + "\n");
+       
+       // Create third expression tree
+       ExprTree tree3 = new ExprTree();
+       tree3.createExprTree(tree3.expression);
+       System.out.println(tree3.expression + " = " 
+               + tree3.evaluateExpression(tree3.root) + "\n");
+    }
+    
+}
