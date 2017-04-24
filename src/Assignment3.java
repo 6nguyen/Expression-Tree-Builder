89b26f155de789d1fe6c6ba917ba0bd7643904fd
George Nguyen <g.nguyen92@yahoo.com>
2017-04-24 14:34:29 -0700

    Add date last modified.

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assignment3;
 
 /**
  *
  * @author George
+ * @since 11/20/2016
  */
 public class Assignment3 {
 
